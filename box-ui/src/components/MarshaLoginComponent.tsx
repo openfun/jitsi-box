82e04737bd976b4d2fbb459392dc38851bbeac36
Maréchal Simon <simon.marechal@student-cs.fr>
2021-11-08 12:48:43 +0100

    add marsha comp

---
+++ box-ui/src/components/MarshaLoginComponent.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import { Button } from '@mui/material';
+import axios from 'axios';
+
+import './MarshaLoginComponent.css';
+
+const MarshaLoginComponent = () => {
+
+    return (
+        <div> 
+            test
+        </div>
+    )
+}
+
+export default MarshaLoginComponent;
