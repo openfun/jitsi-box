eca52e15f59b93d87b9c98f966081ce7eaa782a5
Mohamed Khairallah Gharbi <mohamed-khairallah.gharbi@student-cs.fr>
2021-11-08 12:16:53 +0100

    Formatting joinMeeting

--- box-ui/src/components/JoinMeetingComponent.tsx
+++ box-ui/src/components/JoinMeetingComponent.tsx
@@ -1,15 +1,14 @@
 import { Box, Button } from '@mui/material';
-import {FunctionComponent, useState, useRef} from 'react';
-import './JoinMeetingComponent.css'
-import Keyboard from "react-simple-keyboard";
-import "react-simple-keyboard/build/css/index.css";
+import { FunctionComponent, useState, useRef } from 'react';
+import './JoinMeetingComponent.css';
+import Keyboard from 'react-simple-keyboard';
+import 'react-simple-keyboard/build/css/index.css';
 import HeaderComponent from './HeaderComponent';
 import OutlinedInput from '@mui/material/OutlinedInput';
 import InputLabel from '@mui/material/InputLabel';
 import FormControl from '@mui/material/FormControl';
 
 const JoinMeetingComponent: FunctionComponent = () => {
-
     const [meetingName, setMeetingName] = useState('');
     const [layout, setLayout] = useState('default');
     const keyboard = useRef();
@@ -23,21 +22,21 @@ const JoinMeetingComponent: FunctionComponent = () => {
         setLayout(newLayoutName);
     };
 
-  const onKeyPress = (button: any) => {
-    if (button === "{shift}" || button === "{lock}") handleShift();
-  };
+    const onKeyPress = (button: any) => {
+        if (button === '{shift}' || button === '{lock}') handleShift();
+    };
 
-  const joinRoom = () => {
-    console.log('Launch Jitsi');
-  }
+    const joinRoom = () => {
+        console.log('Launch Jitsi.meeting');
+    };
 
-  const onChangeInput = (event: any) => {
-    setMeetingName(event.target.value);  
-  };
+    const onChangeInput = (event: any) => {
+        setMeetingName(event.target.value);
+    };
     return (
         <div className='JoinMeetingComponent'>
             <div>
-            <HeaderComponent />
+                <HeaderComponent />
             </div>
             <div className='JoinInputContainer'>
                 <div className='JoinMessage'>
@@ -47,27 +46,29 @@ const JoinMeetingComponent: FunctionComponent = () => {
                     <div className='InputMessage'>
                         <Box>
                             <FormControl fullWidth>
-                                <InputLabel htmlFor="outlined-adornment-amount">Room Name/URL</InputLabel>
+                                <InputLabel htmlFor='outlined-adornment-amount'>Room Name/URL</InputLabel>
                                 <OutlinedInput
-                                    id="outlined-adornment-amount"
+                                    id='outlined-adornment-amount'
                                     value={meetingName}
                                     onChange={onChangeInput}
-                                    label="Room Name"
+                                    label='Room Name'
                                 />
                             </FormControl>
                         </Box>
                     </div>
-                    <div className="JoinButton"> 
-                        <Button variant="contained" size="large" onClick={joinRoom} disabled={meetingName.length===0}>Join</Button>
+                    <div className='JoinButton'>
+                        <Button variant='contained' size='large' onClick={joinRoom} disabled={meetingName.length === 0}>
+                            Join
+                        </Button>
                     </div>
                 </div>
                 <div>
-                <Keyboard
-                    keyboardRef={(r:any) => (keyboard.current = r)}
-                    layoutName={layout}
-                    onChange={onChange}
-                    onKeyPress={onKeyPress}
-                />
+                    <Keyboard
+                        keyboardRef={(r: any) => (keyboard.current = r)}
+                        layoutName={layout}
+                        onChange={onChange}
+                        onKeyPress={onKeyPress}
+                    />
                 </div>
             </div>
         </div>
