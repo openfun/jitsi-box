eb3764c37498c1770e33741f69c6c498891081f3
Arthur Naudy <arthur.naudy@student-cs.fr>
2021-11-08 16:02:32 +0100

    added icons  and description for each button

--- box-ui/src/components/DashboardButtonsComponent.tsx
+++ box-ui/src/components/DashboardButtonsComponent.tsx
@@ -1,11 +1,16 @@
 import * as React from 'react';
 import Button from '@mui/material/Button';
 import SliderComponent from './SliderComponent';
-
 import './DashboardButtonsComponent.css'
 
-import mute from '../images/mute.png'
-import camera from '../images/camera.svg'
+import CameraswitchOutlinedIcon from '@mui/icons-material/CameraswitchOutlined';
+import VideoCameraBackOutlinedIcon from '@mui/icons-material/VideoCameraBackOutlined';
+import MicNoneOutlinedIcon from '@mui/icons-material/MicNoneOutlined';
+import MicOffOutlinedIcon from '@mui/icons-material/MicOffOutlined';
+import FiberManualRecordOutlinedIcon from '@mui/icons-material/FiberManualRecordOutlined';
+import ExitToAppOutlinedIcon from '@mui/icons-material/ExitToAppOutlined';
+import PeopleAltOutlinedIcon from '@mui/icons-material/PeopleAltOutlined';
+import ForumOutlinedIcon from '@mui/icons-material/ForumOutlined';
 
 const DashboardButtonsComponent = () => {
 
@@ -15,30 +20,42 @@ const DashboardButtonsComponent = () => {
             <div className='DashboardButtonsRow'>
                 <div className="ButtonContainer">
                     <Button variant="outlined" className="Button"> 
-                        {/* <img src={mute} alt="mute" className='lmj-logo' /> */}
-                        Mute
+                        <MicNoneOutlinedIcon />
                     </Button>
+                    Micro
                 </div>
                 <div className="ButtonContainer">
                     <Button variant="outlined" className="Button">
-                        {/* <img src={camera} alt="start" className='lmj-logo' /> */}
-                        Start/stop video
+                        <VideoCameraBackOutlinedIcon />
                     </Button>
+                    Start/Stop Camera
                 </div>
                 <div className="ButtonContainer">
-                    <Button variant="outlined" className="Button"> Participants </Button>
+                    <Button variant="outlined" className="Button">
+                    <PeopleAltOutlinedIcon />
+                    </Button>
+                    Participants
                 </div>
                 <div className="ButtonContainer">
-                    <Button variant="outlined" className="Button"> Start/Stop Recording </Button>
+                    <Button variant="outlined" className="Button">
+                    <FiberManualRecordOutlinedIcon />
+                    </Button>
+                    Enregistrer
                 </div>
             </div>
 
             <div className='DashboardButtonsRow'>
                 <div className="ButtonContainer">
-                    <Button variant="outlined" className="Button"> Change View </Button>
+                    <Button variant="outlined" className="Button">
+                    <CameraswitchOutlinedIcon />
+                    </Button>
+                    Retour Vidéo
                 </div>
                 <div className="ButtonContainer">
-                    <Button variant="outlined" className="Button"> Chat </Button>
+                    <Button variant="outlined" className="Button">
+                    <ForumOutlinedIcon />
+                    </Button>
+                    Chat
                 </div>
                 <div className="ButtonContainer">
                     <SliderComponent />
@@ -47,7 +64,10 @@ const DashboardButtonsComponent = () => {
             
             <div className='DashboardButtonsRow'>
                 <div className="ButtonContainer">
-                    <Button variant="outlined" className="Button"> Leave </Button>
+                    <Button variant="outlined" className="Button">
+                    <ExitToAppOutlinedIcon />
+                    </Button>
+                    Déconnexion
                 </div>
             </div>
 
