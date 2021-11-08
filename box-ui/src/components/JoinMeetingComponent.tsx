bdd4a0b34828863cc8aa77f65662cf9c73c31b51
Mohamed Khairallah Gharbi <mohamed-khairallah.gharbi@student-cs.fr>
2021-11-08 11:45:56 +0100

    UI Ready for join room

--- box-ui/src/components/JoinMeetingComponent.tsx
+++ box-ui/src/components/JoinMeetingComponent.tsx
@@ -1,27 +1,73 @@
-import { Box, Button, TextField } from '@mui/material';
-import * as React from 'react';
-import './JoinMeetingComponent.css';
+import { Box, Button } from '@mui/material';
+import {FunctionComponent, useState, useRef} from 'react';
+import './JoinMeetingComponent.css'
+import Keyboard from "react-simple-keyboard";
+import "react-simple-keyboard/build/css/index.css";
 import HeaderComponent from './HeaderComponent';
+import OutlinedInput from '@mui/material/OutlinedInput';
+import InputLabel from '@mui/material/InputLabel';
+import FormControl from '@mui/material/FormControl';
 
-const JoinMeetingComponent = () => {
+const JoinMeetingComponent: FunctionComponent = () => {
+
+    const [meetingName, setMeetingName] = useState('');
+    const [layout, setLayout] = useState('default');
+    const keyboard = useRef();
+
+    const onChange = (input: any) => {
+        setMeetingName(input);
+    };
+
+    const handleShift = () => {
+        const newLayoutName = layout === 'default' ? 'shift' : 'default';
+        setLayout(newLayoutName);
+    };
+
+  const onKeyPress = (button: any) => {
+    if (button === "{shift}" || button === "{lock}") handleShift();
+  };
+
+  const joinRoom = () => {
+    console.log('Launch Jitsi');
+  }
+
+  const onChangeInput = (event: any) => {
+    setMeetingName(event.target.value);  
+  };
     return (
         <div className='JoinMeetingComponent'>
             <div>
-                <HeaderComponent />
+            <HeaderComponent />
             </div>
             <div className='JoinInputContainer'>
-                <div>
+                <div className='JoinMessage'>
                     <h2>Enter the link of the Jitsi Meeting</h2>
                 </div>
                 <div className='InputContainer'>
-                    <div>
+                    <div className='InputMessage'>
                         <Box>
-                            <TextField fullWidth label='Meeting link' id='fullWidth' />
+                            <FormControl fullWidth>
+                                <InputLabel htmlFor="outlined-adornment-amount">Room Name/URL</InputLabel>
+                                <OutlinedInput
+                                    id="outlined-adornment-amount"
+                                    value={meetingName}
+                                    onChange={onChangeInput}
+                                    label="Room Name"
+                                />
+                            </FormControl>
                         </Box>
                     </div>
-                    <div>
-                        <Button variant='outlined'>Join</Button>
+                    <div className="JoinButton"> 
+                        <Button variant="contained" size="large" onClick={joinRoom} disabled={meetingName.length===0}>Join</Button>
                     </div>
+                </div>
+                <div>
+                <Keyboard
+                    keyboardRef={(r:any) => (keyboard.current = r)}
+                    layoutName={layout}
+                    onChange={onChange}
+                    onKeyPress={onKeyPress}
+                />
                 </div>
             </div>
         </div>
