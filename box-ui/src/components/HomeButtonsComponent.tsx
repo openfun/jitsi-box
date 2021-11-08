541223eb9783e4202808ed68382e0d406bf47715
Mohamed Khairallah Gharbi <mohamed-khairallah.gharbi@student-cs.fr>
2021-11-08 16:28:49 +0100

    Linking home page to create meeting route

--- box-ui/src/components/HomeButtonsComponent.tsx
+++ box-ui/src/components/HomeButtonsComponent.tsx
@@ -1,12 +1,13 @@
 import * as React from 'react';
 import Button from '@mui/material/Button';
 import './HomeButtonsComponent.css';
+import { Link } from 'react-router-dom';
 const HomeButtonsComponent = () => {
     return (
         <div className='HomeButtonsComponent'>
             <div className='ButtonContainer'>
-                <Button variant='outlined' className='Button'>
-                    Create a meeting{' '}
+                <Button variant='outlined' className='Button' component={Link} to='/create'>
+                    Create a meeting
                 </Button>
             </div>
             <div className='ButtonContainer'>
