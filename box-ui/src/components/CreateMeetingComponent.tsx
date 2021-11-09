252a1876f69cf6dcc9bf6992fa3efc73219842c8
Mohamed Khairallah Gharbi <mohamed-khairallah.gharbi@student-cs.fr>
2021-11-09 10:09:39 +0100

    Adding create component

--- box-ui/src/components/CreateMeetingComponent.tsx
+++ box-ui/src/components/CreateMeetingComponent.tsx
@@ -1,4 +1,6 @@
 import * as React from 'react';
+import { Button } from '@mui/material';
+import { Link } from 'react-router-dom';
 import './CreateMeetingComponent.css';
 import HeaderComponent from './HeaderComponent';
 const CreateMeetingComponent = () => {
@@ -7,7 +9,26 @@ const CreateMeetingComponent = () => {
             <div>
                 <HeaderComponent />
             </div>
-            test Create
+            <div className='CreateMeetingContainer'>
+                <div className='CreateMessage'>
+                    <div>
+                        <h1>Here is your Jitisi Link</h1>
+                    </div>
+                    <div>
+                        <Link to={'meeting.education/test'}>meeting.education/test</Link>
+                    </div>
+                </div>
+                <div className='ButtonContainer'>
+                    <div>
+                        <Button variant='contained' className='Button' component={Link} to='/create'>
+                            Go
+                        </Button>
+                    </div>
+                    <div>
+                        <Link to={''}>I prefer enter the link by myself</Link>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
