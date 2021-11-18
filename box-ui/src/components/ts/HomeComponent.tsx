957c270f420a546c755517b14e7f679dce3f5bae
Arthur Naudy <arthur.naudy@student-cs.fr>
2021-11-18 21:17:17 +0100

    added svg jitsibox logo

--- box-ui/src/components/ts/HomeComponent.tsx
+++ box-ui/src/components/ts/HomeComponent.tsx
@@ -1,11 +1,13 @@
 import React, { FunctionComponent } from 'react';
 import HomeButtonsComponent from './HomeButtonsComponent';
 import '../css/HomeComponent.css';
+import JitsiBoxLogo from '/Users/arthurpdl/Documents/jitsi-box/box-ui/src/logo/jitsibox.svg';
 
 const HomeComponent: FunctionComponent = () => {
     return (
         <div className='Home'>
             <div className='HomeButtonsContainer'>
+                <img src={JitsiBoxLogo} alt='logo de la jitsi-box' className='logo' />
                 <HomeButtonsComponent />
             </div>
         </div>
