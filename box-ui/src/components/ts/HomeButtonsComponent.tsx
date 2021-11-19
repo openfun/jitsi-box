732efbcef548c5c797c61fa26e07830be1a811a8
Arthur Naudy <arthur.naudy@student-cs.fr>
2021-11-19 10:52:03 +0100

    added countdown for returnToMeetingButton & countdown deletes button at 0

--- box-ui/src/components/ts/HomeButtonsComponent.tsx
+++ box-ui/src/components/ts/HomeButtonsComponent.tsx
@@ -1,17 +1,29 @@
-import React, { FunctionComponent } from 'react';
+import React from 'react';
 import Button from '@mui/material/Button';
 import '../css/HomeButtonsComponent.css';
 import { Link } from 'react-router-dom';
-const HomeButtonsComponent: FunctionComponent = () => {
+
+interface CounterProps {
+    counter: number;
+}
+
+const HomeButtonsComponent = (props: CounterProps) => {
+    let button;
+    if (props.counter != 0) {
+        button = (
+            <Button id='ReturnToMeetingButton' variant='contained' className='Button' component={Link} to='/launch'>
+                Revenir dans le meeting
+            </Button>
+        );
+    }
+
     return (
         <div className='HomeButtonsComponent'>
             <div className='ButtonContainer'>
                 <Button id='StartMeetingButton' variant='contained' className='Button' component={Link} to='/launch'>
                     Démarrer un Meeting
                 </Button>
-                <Button id='ReturnToMeetingButton' variant='contained' className='Button' component={Link} to='/launch'>
-                    Revenir dans le meeting
-                </Button>
+                <div>{button}</div>
             </div>
         </div>
     );
--- box-ui/src/components/ts/HomeComponent.tsx
+++ box-ui/src/components/ts/HomeComponent.tsx
@@ -1,14 +1,21 @@
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent, useState } from 'react';
 import HomeButtonsComponent from './HomeButtonsComponent';
 import '../css/HomeComponent.css';
 import JitsiBoxLogo from '/Users/arthurpdl/Documents/jitsi-box/box-ui/src/logo/jitsibox.svg';
 
 const HomeComponent: FunctionComponent = () => {
+    const [counter, setCounter] = useState(10);
+
+    React.useEffect(() => {
+        counter > 0 && setTimeout(() => setCounter(counter - 1), 1000);
+    }, [counter]);
+
     return (
         <div className='Home'>
             <div className='HomeButtonsContainer'>
                 <img src={JitsiBoxLogo} alt='logo de la jitsi-box' className='logo' />
-                <HomeButtonsComponent />
+                <div>{counter}</div>
+                <HomeButtonsComponent counter={counter} />
             </div>
         </div>
     );
