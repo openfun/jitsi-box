9ac9d3383931cff6a5e8fc37502955202c5e85d6
Mohamed Khairallah Gharbi <mohamed-khairallah.gharbi@student-cs.fr>
2021-11-22 10:21:37 +0100

    Add QRCode window

--- box-ui/src/components/ts/QrCodeComponent.tsx
+++ box-ui/src/components/ts/QrCodeComponent.tsx
@@ -3,7 +3,8 @@ import React, { FunctionComponent, useState, ChangeEvent, useRef } from 'react';
 import '../css/QrCodeComponent.css';
 import 'react-simple-keyboard/build/css/index.css';
 import QRCode from 'react-qr-code';
-
+import QrCodeIcon from '@mui/icons-material/QrCode';
+import PhoneIphoneIcon from '@mui/icons-material/PhoneIphone';
 interface InputRoom {
     domain: string;
     roomName: string;
@@ -15,7 +16,30 @@ const QrCodeComponent: FunctionComponent<InputRoom> = ({ domain: domain, roomNam
             <div className='QRItem'>
                 <QRCode value={`https://${domain}/${roomName}`} size={320} />
             </div>
-            <div className='DetailsContainer'> information</div>
+            <div className='DetailsContainer'>
+                <div className='ParagraphContainer'>
+                    <div>
+                        <QrCodeIcon style={{ height: '35px', width: '35px' }} />
+                    </div>
+                    <div className='ParagraphItem'>
+                        <p>
+                            En flashant ce QR code, vous pouvez ajouter facilement une caméra à la conférence avec votre
+                            smartphone ou envoyer l&apos;adresse de la conférence à un participant.
+                        </p>
+                    </div>
+                </div>
+                <div className='ParagraphContainer'>
+                    <div>
+                        <PhoneIphoneIcon style={{ height: '35px', width: '35px' }} />
+                    </div>
+                    <div className='ParagraphItem'>
+                        <p>
+                            <strong>Astuce:</strong> pensez à utiliser votre smartphone pour le Chat ou pour partager le
+                            QR code
+                        </p>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
