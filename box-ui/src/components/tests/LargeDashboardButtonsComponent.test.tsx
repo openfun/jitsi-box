f523cca4b09401e1b137a19798e3723b15899d7e
Mohamed Khairallah Gharbi <mohamed-khairallah.gharbi@student-cs.fr>
2021-11-26 15:41:40 +0100

    Remove unused files

--- box-ui/src/components/tests/LargeDashboardButtonsComponent.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import LargeDashboardButtonsComponent from '../ts/LargeDashboardButtonsComponent';
-
-test('renders learn react link', () => {
-    render(<LargeDashboardButtonsComponent />);
-    const linkElement = screen.getByText(/learn react/i);
-    expect(linkElement).toBeInTheDocument();
-});
