70d4a128ed5f8b1cf00b2aa245b30544a153a949
Mohamed Khairallah Gharbi <mohamed-khairallah.gharbi@student-cs.fr>
2021-12-03 16:16:45 +0100

    Updating after rebase

--- box-ui/src/components/ts/JitsiComponent.tsx
+++ box-ui/src/components/ts/JitsiComponent.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState, FunctionComponent } from 'react';
-import { NavigateFunction, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { IconButton } from '@mui/material';
 import CallEndIcon from '@mui/icons-material/CallEnd';
 import '../css/JitsiComponent.css';
