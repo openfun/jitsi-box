3988d11443af7df30f23074ca5c7098ed61280e3
aladdin <aladdin@MacBook-Pro-de-aladdin.local>
2022-02-25 16:55:31 +0100

    new component that list all cameras of the device, it is called in JitsiComponent

---
+++ box-ui/src/components/ts/CameraDetectorComponent.tsx
@@ -0,0 +1,33 @@
+import React, { useState, useEffect, useRef } from 'react';
+import CaptureImage from './CaptureImageComponent';
+
+const CameraDetector = () => {
+    const [cameraList, setCameraList] = useState([] as any[]);
+    const selectCamera = () => {
+        navigator.mediaDevices.enumerateDevices().then((devices: Array<any>) => {
+            setCameraList(devices.filter((value) => value['kind'] === 'videoinput'));
+        });
+    };
+    useEffect(() => {
+        selectCamera();
+    }, []);
+    useEffect(() => {
+        navigator.mediaDevices.addEventListener('devicechange', selectCamera);
+        return () => {
+            navigator.mediaDevices.removeEventListener('devicechange', selectCamera);
+        };
+    }, []);
+    return (
+        <div>
+            {cameraList.map((element) => {
+                return (
+                    <div className={element['label']} key={element['deviceId']}>
+                        <CaptureImage camera={element} />
+                    </div>
+                );
+            })}
+        </div>
+    );
+};
+
+export default CameraDetector;
