1b98d7630cec7b903b62e732ef064eef28f084a7
Vianney Ruhlmann <85412254+djexvr@users.noreply.github.com>
2022-03-07 10:04:37 +0100

    Refactor jitsi component (#62)
    * 🏷️ (JitsiMeetAPI) Add options to config typing
    
    Add config options from jitsi-meet documentation to type declaration
    to allow the use of typing when using the api.
    
    * ♻️ (JitsiMeetComponent) refactor jitsi component
    
    Refactor component to expose the JitsiMeetAPI to be able to reuse
    the functions in the different interfaces.
    
    Remove the custom hangup button as it is possible to modify
    the default button behavior through listener.
    
    Add types for the api to make the component personalisation easier.
    
    * ✏️ (types) fix typos in jitsi api config types
    
    * 👽️ (JitsiComponent) replace deprecated config options
    
    Some options from InterfaceConfig are deprecated and have been replaced
    by their equivalents in config.
    
    * ♻️  (Jitsi Frame) allow configuration of the jitsi frame
    
    Rename the Jitsi Component to Jitsi Frame to be more explicit.
    
    Allow the configuration of the Jitsi Frame to apply different options
    and access Jitsi api through the configure prop.
    This should simplify the creation of different interfaces.
    
    Co-authored-by: Vianney Ruhlmann <vianney.ruhlmann@student-cs.fr>

--- box-ui/src/components/ts/CreateMeetingComponent.tsx
+++ box-ui/src/components/ts/CreateMeetingComponent.tsx
@@ -1,23 +1,67 @@
 import React, { useState, FunctionComponent } from 'react';
-import { useLocation } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 import '../css/CreateMeetingComponent.css';
-import JitsiComponent from './JitsiComponent';
 import PopupComponent from './PopupComponent';
 import { LocationState } from '../../utils/State';
+import JitsiFrame from './JitsiFrame';
 
 const CreateMeetingComponent: FunctionComponent = () => {
+    const navigate = useNavigate();
     const state = useLocation().state as LocationState;
     const [information, setInformation] = useState({
         roomName: state && state.roomName ? state.roomName : 'dty',
         domain: state && state.domain ? state.domain : 'meeting.education',
     });
+    const meetingOptions = {
+        userInfo: {
+            email: '',
+            displayName: 'Jitsi-Box',
+        },
+        configOverwrite: {
+            toolbarButtons: [
+                'microphone',
+                'camera',
+                'videoquality',
+                'fodeviceselection',
+                'raisehand',
+                'tileview',
+                'hangup',
+            ],
+            toolbarConfig: {
+                alwaysVisible: true,
+            },
+            prejoinConfig: {
+                enabled: false,
+            },
+            startWithVideoMuted: false,
+            startWithAudioMuted: true,
+        },
+    };
 
     return (
         <div className='CreateMeetingComponent'>
             <PopupComponent information={information} setInformation={setInformation} />
             <div className='CreateMeetingContainer'>
                 <div className='JitsiComponent'>
-                    <JitsiComponent information={information} />
+                    <JitsiFrame
+                        information={information}
+                        options={meetingOptions}
+                        configure={(api) => {
+                            api.addListener('videoConferenceLeft', () => {
+                                navigate('/', {
+                                    replace: true,
+                                    state: {
+                                        count: 120,
+                                        roomName: information.roomName,
+                                        domain: information.domain,
+                                    },
+                                });
+                            });
+                        }}
+                        onError={() => {
+                            navigate('/');
+                        }}
+                    />
                 </div>
             </div>
         </div>
---
+++ box-ui/src/components/ts/JitsiFrame.tsx
@@ -0,0 +1,73 @@
+import React, { useEffect, FunctionComponent } from 'react';
+import '../css/JitsiComponent.css';
+import { JitsiFrameProps } from '../../utils/Props';
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import JitsiMeetExternalAPI from '../../utils/JitsiMeetExternalAPI';
+
+const loadJitsiScript = (url: string) =>
+    new Promise((resolve, reject) => {
+        document.getElementById('jitsi-api-script')?.remove();
+        const script = document.createElement('script');
+        script.setAttribute('id', 'jitsi-api-script');
+        script.src = url;
+        script.onload = resolve;
+        script.onerror = reject;
+        document.body.appendChild(script);
+    });
+
+const instantiateJitsi = async (
+    domainFromProps: string,
+    options: ConstructorParameters<typeof JitsiMeetExternalAPI>[1] = {},
+): Promise<JitsiMeetExternalAPI> => {
+    await loadJitsiScript(`https://${domainFromProps}/external_api.js`);
+    return new window.JitsiMeetExternalAPI(domainFromProps, options) as JitsiMeetExternalAPI;
+};
+
+const JitsiFrame: FunctionComponent<JitsiFrameProps> = (props: JitsiFrameProps) => {
+    useEffect(() => {
+        const defaultOptions: ConstructorParameters<typeof JitsiMeetExternalAPI>[1] = {
+            roomName: props.information.roomName,
+            parentNode: document.getElementById('jitsi-container') || undefined,
+            userInfo: {
+                email: '',
+                displayName: 'Unknown',
+            },
+            interfaceConfigOverwrite: {
+                MOBILE_APP_PROMO: false,
+                SHOW_CHROME_EXTENSION_BANNER: false,
+                DISABLE_JOIN_LEAVE_NOTIFICATIONS: true,
+                SHOW_DEEP_LINKING_IMAGE: false,
+            },
+            configOverwrite: {
+                disableDeepLinking: true,
+                enableWelcomePage: false,
+            },
+        };
+
+        const apiPromise = instantiateJitsi(props.information.domain, { ...defaultOptions, ...props.options });
+        apiPromise
+            .then((api) => {
+                api.addListener('videoConferenceLeft', () => {
+                    api.dispose();
+                });
+                props.configure?.(api);
+            })
+            .catch(() => {
+                alert('Error loading Jitsi Meet API');
+                props.onError?.();
+            });
+        return () => {
+            apiPromise.then((api) => {
+                api.dispose();
+            });
+        };
+    }, [props.information, props.options, props.configure, props.onError]);
+
+    return (
+        <div style={{ height: '100%' }}>
+            <div id='jitsi-container' className='jitsiContainer' />
+        </div>
+    );
+};
+
+export default JitsiFrame;
