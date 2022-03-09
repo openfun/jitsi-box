9bb8e4cb882e0c8a34065194a8b8c53e7ec6f1a9
Vianney Ruhlmann <85412254+djexvr@users.noreply.github.com>
2022-03-09 14:27:31 +0100

    Reorganize routes (#63)
    * 🚚 (route): refactor component organisation
    
    Prepare for the insertion of other interface and refactor components
    organisation to distinguish the different interface.
    
    For improved readability the new components do not include "Component"
    in their names.
    
    * ✨(route) add teacher and students interface routes
    
    Add routes and boilerplate components for teacher and students interface.
    The students interface is considered as the default one since they are the most common
    kind of user.
    
    Co-authored-by: Vianney Ruhlmann <vianney.ruhlmann@student-cs.fr>

--- box-ui/src/App.tsx
+++ box-ui/src/App.tsx
@@ -1,16 +1,31 @@
 import React, { FunctionComponent } from 'react';
 import { BrowserRouter as Router, Navigate, Route, Routes } from 'react-router-dom';
-import CreateMeetingComponent from './components/ts/CreateMeetingComponent';
-import HomeComponent from './components/ts/HomeComponent';
+import BoxHome from './components/ts/Box/BoxHome';
+import BoxMeeting from './components/ts/Box/BoxMeeting';
+import StudentHome from './components/ts/Student/StudentMeeting';
+import StudentMeeting from './components/ts/Student/StudentHome';
+import TeacherHome from './components/ts/Teacher/TeacherHome';
+import TeacherMeeting from './components/ts/Teacher/TeacherMeeting';
 
 const App: FunctionComponent = () => {
     return (
         <Router>
             <div className='App'>
                 <Routes>
-                    <Route path='/' element={<HomeComponent />} />
-                    <Route path='/launch' element={<CreateMeetingComponent />} />
-                    <Route path='*' element={<Navigate to='/' />} />
+                    <Route path='/' element={<Navigate to='/student' />} />
+                    <Route path='box'>
+                        <Route index element={<BoxHome />} />
+                        <Route path='meeting' element={<BoxMeeting />} />
+                    </Route>
+                    <Route path='teacher'>
+                        <Route index element={<TeacherHome />} />
+                        <Route path='meeting' element={<TeacherMeeting />} />
+                    </Route>
+                    <Route path='student'>
+                        <Route index element={<StudentHome />} />
+                        <Route path='meeting' element={<StudentMeeting />} />
+                    </Route>
+                    <Route path='*' element={<Navigate to='student' />} />
                 </Routes>
             </div>
         </Router>
---
+++ box-ui/src/components/ts/Teacher/TeacherMeeting.tsx
@@ -0,0 +1,7 @@
+import React, { FunctionComponent } from 'react';
+
+const TeacherMeeting: FunctionComponent = () => {
+    return <p>TeacherMeeting</p>;
+};
+
+export default TeacherMeeting;
