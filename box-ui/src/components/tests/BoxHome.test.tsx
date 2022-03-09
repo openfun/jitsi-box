3bba5458e984b010078909e6af1466fc0c3f2d8d
Vianney Ruhlmann <85412254+djexvr@users.noreply.github.com>
2022-03-09 14:27:31 +0100

    Reorganize routes (#63)
    * 🚚 (route): refactor component organisation
    
    Prepare for the insertion of other interface and refactor components
    organisation to distinguish the different interface.
    
    For improved readability the new components do not include "Component"
    in their names.
    
    * ✨(route) add teacher and students interface routes
    
    Add routes and boilerplate components for teacher and students interface.
    The students interface is considered as the default one since they are the most common
    kind of user.
    
    Co-authored-by: Vianney Ruhlmann <vianney.ruhlmann@student-cs.fr>

--- box-ui/src/components/tests/BoxHome.test.tsx
+++ box-ui/src/components/tests/BoxHome.test.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import CreateMeetingComponent from '../ts/CreateMeetingComponent';
+import BoxHome from '../ts/Box/BoxHome';
 
 test('renders learn react link', () => {
-    render(<CreateMeetingComponent />);
+    render(<BoxHome />);
     const linkElement = screen.getByText(/learn react/i);
     expect(linkElement).toBeInTheDocument();
 });
