5ae4bd40620d37f5970ff759d254cd35cdd46fb1
faydou <88634802+faydou@users.noreply.github.com>
2022-03-14 09:40:30 +0100

    ✨ Makes sure the screen flashes when a hand is raised and automatically turns off (#57)
    * Added Raised Hand detection with surrounding yellow boders
    
    * got rid of ugly color show on start
    
    * changed bright contour to full screen opaque curtain
    
    * clean functionality
    
    * changes hook to useref instead of usedtate for better preformance
    
    * refresh problem
    
    * patches rerender problem for demo
    
    * implements suggestions
    
    Co-authored-by: Fayssal Defaa <faydef@MacBook-Pro-de-Fayssal.local>
    Co-authored-by: Fayssal Defaa <defaa.fayssal.mp@gmail.com>

--- box-ui/src/utils/Props/index.d.ts
+++ box-ui/src/utils/Props/index.d.ts
@@ -37,6 +37,7 @@ interface InformationProps {
 
 export type JitsiFrameProps = {
     information: Information;
+    isBox : boolean;
     options: ConstructorParameters<typeof JitsiMeetExternalAPI>[1];
     configure?: (api: JitsiMeetExternalAPI) => void;
     onError?: () => void;
