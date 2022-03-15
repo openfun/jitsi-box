8433e3429381ef6ba1e117f3457be0aaa847a800
djexvr <ruhlmannvianney@gmail.com>
2022-03-15 11:20:15 +0100

    ♻️  (BoxHome) use reusable components
    Replace legacy components with reusable ones.
    Handle possibly null state.

--- box-ui/src/components/ts/Box/BoxHome.tsx
+++ box-ui/src/components/ts/Box/BoxHome.tsx
@@ -1,36 +1,36 @@
-import React, { FunctionComponent, useState, useEffect } from 'react';
-import HomeButtonsComponent from '../HomeButtonsComponent';
+import React, { FunctionComponent } from 'react';
 import HomeButton from '../HomeButton';
-import '../../css/BoxHome.css';
+import '../../css/Home.css';
 import JitsiBoxLogo from '../../../logo/jitsibox.svg';
 import LanguageSelector from '../LanguageSelector';
 import { useLocation, useNavigate } from 'react-router-dom';
 import { LocationState } from '../../../utils/State';
 import { useTranslation } from 'react-i18next';
 import GenerateRandomFrenchRoomName from '../../../dictionnaries_fr';
+import ProgressButton from '../ProgressButton';
 
 const BoxHome: FunctionComponent = () => {
-    const state = useLocation().state as LocationState;
+    const state = useLocation().state as LocationState | null;
     const { t } = useTranslation();
     const navigate = useNavigate();
-    const [counter, setCounter] = useState(state && state.count ? state.count : 0);
-    const [information] = useState({
-        roomName: state && state.roomName ? state.roomName : '',
-        domain: state && state.domain ? state.domain : '',
-    });
     const launchMeeting = () => {
         navigate(
             { pathname: '/box/meeting' },
             {
-                replace: true,
                 state: { roomName: GenerateRandomFrenchRoomName(), domain: 'meeting.education' },
             },
         );
     };
 
-    useEffect(() => {
-        counter > 0 && setTimeout(() => setCounter(counter - 1), 1000);
-    }, [counter]);
+    const launchLastMeeting = () => {
+        navigate(
+            { pathname: '/box/meeting' },
+            {
+                state: { roomName: state ? state.roomName : '', domain: state ? state.roomName : '' },
+            },
+        );
+    };
+
     return (
         <div className='HomeContainer'>
             <div className='LogoContainer'>
@@ -40,7 +40,11 @@ const BoxHome: FunctionComponent = () => {
                 <HomeButton id='StartMeetingButton' onClick={launchMeeting} variant='contained'>
                     {t('launchMeeting')}
                 </HomeButton>
-                <HomeButtonsComponent counter={counter} roomName={information.roomName} domain={information.domain} />
+                <ProgressButton initialCounter={state ? state.count : 0} onClick={launchLastMeeting}>
+                    {t('goBackToMeeting')}
+                    <br />
+                    {state ? state.roomName : ''}
+                </ProgressButton>
             </div>
             <LanguageSelector />
         </div>
