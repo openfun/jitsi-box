2c4952086ccbca5ac339913111c17a63e4871c34
djexvr <ruhlmannvianney@gmail.com>
2022-03-31 11:42:11 +0200

    💄(Home) update button dimensions
    The dimensions of the ReturnToLastMeeting button is not
    suitable for student and teacher interface.

--- box-ui/src/components/ts/Teacher/TeacherHome.tsx
+++ box-ui/src/components/ts/Teacher/TeacherHome.tsx
@@ -41,7 +41,7 @@ const TeacherHome: FunctionComponent = () => {
             <div className='LogoContainer'>
                 <img src={JitsiBoxLogo} alt='logo de la jitsi-box' className='logo' />
             </div>
-            <h2 style={{ color: '#505050' }}>{t('teacherSpace')}</h2>
+            <h2>{t('teacherSpace')}</h2>
             <div className='HomeButtonsContainer'>
                 <InputButton buttonLabel={t('joinAMeeting')} inputLabel={t('enterRoomName')} onSubmit={joinMeeting} />
                 <ProgressButton initialCounter={state ? state.count : 0} onClick={launchLastMeeting}>
